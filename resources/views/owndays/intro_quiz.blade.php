6d0341659245e47a0e3656725fd0390c2e713fb6
kimkundad <kim.kundad@gmail.com>
2025-10-14 04:16:16 +0700

    kim

--- resources/views/owndays/intro_quiz.blade.php
+++ resources/views/owndays/intro_quiz.blade.php
@@ -41,10 +41,9 @@
         <a href="{{ url('/quiz') }}" class="start-btn">
                                 <img src="{{ url('img/owndays/start.png') }}" alt="พร้อมแล้ว ไปต่อกันเลย"
                                     class="btn-image">
-                                    <br><br>
                             </a>
 
-                            <br><br>
+
 
 
       </div>
